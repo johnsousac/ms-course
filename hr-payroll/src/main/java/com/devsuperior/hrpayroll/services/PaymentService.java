baa044381afded6b99684783e5ca9d5823851747
John Castro de Sousa <37676163+johnsousac@users.noreply.github.com>
2023-10-30 23:10:14 -0300

    Feign client

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
@@ -4,23 +4,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.devsuperior.hrpayroll.entities.Payment;
 import com.devsuperior.hrpayroll.entities.Worker;
+import com.devsuperior.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 	
-	// recuperar valor de atributo declarado
-	// no arquivo application.properties
-	@Value("${hr-worker.host}")
-	private String workerHost;
-	
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
 
 	/**
 	 * metodo responsável por devolver o pagamento do trabalhador
@@ -30,19 +24,8 @@ public class PaymentService {
 	 * @return
 	 */
 	public Payment getPayment(long workerId, int days) {
-		
-		// preparando parametros da requisição
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", Long.toString(workerId));
-		
-		// realizando requisição com rest template
-		// {id} sera mapeado com a chave id no Map
-		// uriVariables
-		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", 
-				Worker.class, uriVariables);
-		
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
-		
 	}
 	
 }
