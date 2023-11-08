3f7721bb1ce098e06762e2561d898aab7e9a9112
John Castro de Sousa <37676163+johnsousac@users.noreply.github.com>
2023-11-08 09:15:37 -0300

    Security configs to config server

--- hr-api-gateway-zuul/src/main/java/com/devsuperior/hrapigatewayzuul/config/ResourceServerConfig.java
+++ hr-api-gateway-zuul/src/main/java/com/devsuperior/hrapigatewayzuul/config/ResourceServerConfig.java
@@ -18,7 +18,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	
 	private static final String[] PUBLIC = { "/hr-oauth/oauth/token" };
 	private static final String[] OPERATOR = { "/hr-worker/**" };
-	private static final String[] ADMIN = { "/hr-payroll/**", "/hr-user/**" };
+	private static final String[] ADMIN = { "/hr-payroll/**", "/hr-user/**", "/actuator/**", "/hr-worker/actuator/**", "/hr-payroll/actuator/**" };
 	
 	// metodo para ler o token
 	@Override
